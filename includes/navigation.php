5e0bcb38d1e8d5663837ef8ef8ab708218bab2d4
Subham <subhamiqwing@gmail.com>
2024-02-03 12:16:54 +0530

    changed nav

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,204 +1,63 @@
-<header>
-    <div class="container-fluid px-0">
-        <nav class="navbar navbar-expand-md navbar-light bg-white p-0">
-            <a class="navbar-brand mr-4" href="#"><strong>BBBootstrap</strong></a>
+<div class="MobileNav" data-mobile-nav>
+    <div class="MobileNav-overlay" data-mobile-nav-overlay></div>
+    <div class="MobileNav-inner">
+        <div class="MobileNav-trigger" data-mobile-nav-trigger>
+            <div class="MobileNav-trigger-inner"></div>
+        </div>
+        <ul class="MobileNav-nav" data-mobile-nav-list>
+            <li class="MobileNav-item" data-mobile-nav-item="1">
+                <div class="MobileNav-item-title">
+                    <a href="#">About</a>
+                </div>
+            </li>
+            <li class="MobileNav-item" data-mobile-nav-item="2">
+                <div class="MobileNav-item-title">
+                    <a href="#">Products</a>
+                </div>
+            </li>
+            <li class="MobileNav-item" data-mobile-nav-item="3">
+                <div class="MobileNav-item-title">
+                    <a href="#">Services</a>
+                </div>
+            </li>
+            <li class="MobileNav-item" data-mobile-nav-item="4">
+                <div class="MobileNav-item-title">
+                    <a href="#">Work</a>
+                </div>
+            </li>
+            <li class="MobileNav-item" data-mobile-nav-item="5">
+                <div class="MobileNav-item-title">
+                    <a href="#">Contact</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
 
-            <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Products<span class="fa fa-angle-down"></span></a>
-                        <div class="dropdown-menu" id="dropdown-menu1" aria-labelledby="navbarDropdown1">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-shopping-cart"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">WearCMS</h6>
-                                                    <small class="text-muted">For your project</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-gamepad"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">Game+</h6>
-                                                    <small class="text-muted">Monetization of games</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-video-camera"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">Streetcam</h6>
-                                                    <small class="text-muted">Keep track all year</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-comment"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">Teamne</h6>
-                                                    <small class="text-muted">Teamwork</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-briefcase"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">Prospec</h6>
-                                                    <small class="text-muted">Solutions for your business</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-bolt"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">Booster</h6>
-                                                    <small class="text-muted">Increase engagement</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Resources<span class="fa fa-angle-down"></span></a>
-                        <div class="dropdown-menu" id="dropdown-menu2" aria-labelledby="navbarDropdown2">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-folder"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">WhitePaper</h6>
-                                                    <small class="text-muted">Marketing and report</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-question"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">FAQs</h6>
-                                                    <small class="text-muted">Qs and answers</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-calculator"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">Tools</h6>
-                                                    <small class="text-muted">All tools</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-paper-plane"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">Success Stories</h6>
-                                                    <small class="text-muted">Experiences</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Customers<span class="fa fa-angle-down"></span></a>
-                        <div class="dropdown-menu" id="dropdown-menu3" aria-labelledby="navbarDropdown3">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-feed"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">Feedback</h6>
-                                                    <small class="text-muted">Opinions, complaints</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="row d-flex tab">
-                                            <div class="fa-icon text-center">
-                                                <span class="fa fa-question"></span>
-                                            </div>
-                                            <div class="d-flex flex-column">
-                                                <a class="dropdown-item" href="#">
-                                                    <h6 class="mb-0">FAQs</h6>
-                                                    <small class="text-muted">Qs and answers</small>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
+<header class="Header" data-header>
+    <div class="Header-inner" data-header-nav>
+        <div class="logo">
+            <img src="https://i.imgur.com/F0k8l7N.png" alt="">
+        </div>
+        <nav class="Header-nav" data-nav>
+            <div class="Header-nav-item" data-nav-item="1">
+                <a href="#">About</a>
+            </div>
+            <div class="Header-nav-item" data-nav-item="2">
+                <a href="#">Products</a>
+            </div>
+            <div class="Header-nav-item" data-nav-item="3">
+                <a href="#">Services</a>
+            </div>
+            <div class="Header-nav-item" data-nav-item="4">
+                <a href="#">Work</a>
+            </div>
+            <div class="Header-nav-item" data-nav-item="5">
+                <a href="#">Contact</a>
             </div>
         </nav>
+        <button class="MobileNav-trigger" data-mobile-nav-trigger>
+            <div class="MobileNav-trigger-inner"></div>
+        </button>
     </div>
 </header>
